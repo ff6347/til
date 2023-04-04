a75bc68392070e7cee864282cd32fd1148843d95
ff6347 <hash@ff6347.email>
2023-04-04 21:14:40 +0200

    test: test write function

--- main_test.go
+++ main_test.go
@@ -14,6 +14,46 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func Test_saveToCsvFile(t *testing.T) {
+    // Create a temporary CSV file
+    fileName := "test_output_save.csv"
+    dataPath, _ := xdg.DataFile(filepath.Join("til", fileName))
+
+    // Content to be written
+    data := [][]string{
+        {"2022-01-01 00:00:00", "Test entry 1"},
+        {"2022-01-02 00:00:00", "Test entry 2"},
+    }
+
+    // Call the function to test it
+    err := saveToCsvFile(dataPath, data)
+    require.NoError(t, err)
+
+    // Read the content from the temporary CSV file
+    var buffer bytes.Buffer
+    f, err := os.Open(dataPath)
+    require.NoError(t, err)
+    _, err = buffer.ReadFrom(f)
+    require.NoError(t, err)
+    _ = f.Close()
+
+    //////
+    // Updated expected content of the file (removed quotes)
+    var expected bytes.Buffer
+    expected.WriteString("2022-01-01 00:00:00,Test entry 1\n")
+    expected.WriteString("2022-01-02 00:00:00,Test entry 2\n")
+    //////
+    
+    expectedStr := expected.String()
+    outputStr := buffer.String()
+    assert.Equal(t, expectedStr, outputStr) // assert.Equal will check if the expected output is equal to the received output
+
+    // Cleanup the temporary file
+    err = os.Remove(dataPath)
+    require.NoError(t, err) // require.NoError will stop the test execution if there's an error
+}
+
+
 func Test_listCsvContents(t *testing.T) {
 	// Create a temporary CSV file
 	fileName := "test_output.csv"
@@ -69,14 +109,22 @@ func captureOutput(f func()) string {
 	return buf.String()
 }
 
+
 func TestMain(m *testing.M) {
-    retCode := m.Run()
+	retCode := m.Run()
 
-    // cleanup
-    fileName := "test_output.csv"
-    dataPath, _ := xdg.DataFile(filepath.Join("til", fileName))
-    os.Remove(dataPath)
+	// Cleanup
+	fileNames := []string{
+		"test_output.csv",
+		"test_output_save.csv",
+	}
 
-    os.Exit(retCode)
+	for _, fileName := range fileNames {
+		dataPath, _ := xdg.DataFile(filepath.Join("til", fileName))
+		os.Remove(dataPath)
+	}
+
+	os.Exit(retCode)
 }
+
 
