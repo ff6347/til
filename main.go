689a7b6e790305a185a8beadfdeaa1cab4138900
ff6347 <hash@ff6347.email>
2023-04-04 21:48:52 +0200

    feat: Add short flags and outfile path log

--- main.go
+++ main.go
@@ -18,17 +18,46 @@ import (
 func main() {
 
 	//Parse CLI arguments
-	fileName := flag.String("filename", "output.csv", "Output file name")
-	listFlag := flag.Bool("list", false, "List all TILs")
-
+	fileName := flag.String("f", "output.csv", "Output file name (alias for --filename)")
+	fileNameLong := flag.String("filename", "", "Output file name")
+	listFlag := flag.Bool("l", false, "List all TILs (alias for --list)")
+	listFlagLong := flag.Bool("list", false, "List all TILs")
+	helpFlag := flag.Bool("h", false, "Show help (alias for --help)")
+	helpFlagLong := flag.Bool("help", false, "Show help")
+	printPathFlagLong := flag.Bool("print-path", false, "Print path to the default output file")
+	printPathFlag := flag.Bool("p", false, "Print path to the default output file (alias for --print-path)")
 	flag.Parse()
 
+	if *fileNameLong != "" {
+		*fileName = *fileNameLong
+	}
+
+	if *listFlagLong {
+		*listFlag = true
+	}
+
+	if *helpFlagLong {
+		*helpFlag = true
+	}
+	if *printPathFlagLong {
+		*printPathFlag = true
+	}
+
 	dataPath, err := xdg.DataFile(filepath.Join("til", *fileName))
+
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
+	if *helpFlag {
+		showHelp()
+		return
+	}
+	if *printPathFlag {
+		printDefaultFilePath(*fileName)
+		return
+	}
 	// list all the TILs
 
 	if *listFlag {
@@ -56,7 +85,7 @@ func main() {
 			// if line == "" {
 			// 	break
 			// }
-			// timestamp := stamp() 
+			// timestamp := stamp()
 			data = append(data, []string{stamp(), strings.TrimSpace(line)})
 		}
 
@@ -79,7 +108,7 @@ func main() {
 			lines = append(lines, strings.TrimSpace(line))
 
 		}
-		// timestamp := stamp() 
+		// timestamp := stamp()
 		completeText := strings.Join(lines, "\\n")
 		data = append(data, []string{stamp(), completeText})
 
@@ -102,10 +131,36 @@ func main() {
 	fmt.Printf("Text saved to %s\n", dataPath)
 }
 
-func stamp () string {
-  return time.Now().Format("2006-01-02 15:04:05") 
+func stamp() string {
+	return time.Now().Format("2006-01-02 15:04:05")
 }
+func showHelp() {
+	helpText := `
+TIL is a simple command-line tool for storing and listing short learnings.
 
+Usage:
+  til [flags] [--] [arguments ...]
+
+Flags:
+  -f, --filename      Set output file name (default: "output.csv")
+  -l, --list          List all TILs
+  -h, --help          Show help
+  -p, --print-path    Print path to the default output file
+
+Examples:
+  1. Read input from the user and store it:
+     til -f output.csv
+
+  2. Read input from pipe:
+     echo "Piped input" | til
+
+  3. List all the contents from the CSV file:
+     til -l
+
+  4. Print the path to the default output file:
+     til --print-path  `
+	fmt.Println(helpText)
+}
 func listCsvContents(filePath string) error {
 
 	f, err := os.Open(filePath)
@@ -126,7 +181,14 @@ func listCsvContents(filePath string) error {
 	return nil
 }
 
-
+func printDefaultFilePath(filename string) {
+	dataPath, err := xdg.DataFile(filepath.Join("til", filename))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Default output file path:", dataPath)
+}
 func saveToCsvFile(filePath string, content [][]string) error {
 	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ import (
 	//"strings"
 	"testing"
 	//"time"
-  "io"
+	"io"
 
 	"github.com/adrg/xdg"
 	"github.com/stretchr/testify/assert"
@@ -15,44 +15,43 @@ import (
 )
 
 func Test_saveToCsvFile(t *testing.T) {
-    // Create a temporary CSV file
-    fileName := "test_output_save.csv"
-    dataPath, _ := xdg.DataFile(filepath.Join("til", fileName))
+	// Create a temporary CSV file
+	fileName := "test_output_save.csv"
+	dataPath, _ := xdg.DataFile(filepath.Join("til", fileName))
 
-    // Content to be written
-    data := [][]string{
-        {"2022-01-01 00:00:00", "Test entry 1"},
-        {"2022-01-02 00:00:00", "Test entry 2"},
-    }
+	// Content to be written
+	data := [][]string{
+		{"2022-01-01 00:00:00", "Test entry 1"},
+		{"2022-01-02 00:00:00", "Test entry 2"},
+	}
 
-    // Call the function to test it
-    err := saveToCsvFile(dataPath, data)
-    require.NoError(t, err)
+	// Call the function to test it
+	err := saveToCsvFile(dataPath, data)
+	require.NoError(t, err)
 
-    // Read the content from the temporary CSV file
-    var buffer bytes.Buffer
-    f, err := os.Open(dataPath)
-    require.NoError(t, err)
-    _, err = buffer.ReadFrom(f)
-    require.NoError(t, err)
-    _ = f.Close()
+	// Read the content from the temporary CSV file
+	var buffer bytes.Buffer
+	f, err := os.Open(dataPath)
+	require.NoError(t, err)
+	_, err = buffer.ReadFrom(f)
+	require.NoError(t, err)
+	_ = f.Close()
 
-    //////
-    // Updated expected content of the file (removed quotes)
-    var expected bytes.Buffer
-    expected.WriteString("2022-01-01 00:00:00,Test entry 1\n")
-    expected.WriteString("2022-01-02 00:00:00,Test entry 2\n")
-    //////
-    
-    expectedStr := expected.String()
-    outputStr := buffer.String()
-    assert.Equal(t, expectedStr, outputStr) // assert.Equal will check if the expected output is equal to the received output
+	//////
+	// Updated expected content of the file (removed quotes)
+	var expected bytes.Buffer
+	expected.WriteString("2022-01-01 00:00:00,Test entry 1\n")
+	expected.WriteString("2022-01-02 00:00:00,Test entry 2\n")
+	//////
 
-    // Cleanup the temporary file
-    err = os.Remove(dataPath)
-    require.NoError(t, err) // require.NoError will stop the test execution if there's an error
+	expectedStr := expected.String()
+	outputStr := buffer.String()
+	assert.Equal(t, expectedStr, outputStr) // assert.Equal will check if the expected output is equal to the received output
+
+	// Cleanup the temporary file
+	err = os.Remove(dataPath)
+	require.NoError(t, err) // require.NoError will stop the test execution if there's an error
 }
-
 
 func Test_listCsvContents(t *testing.T) {
 	// Create a temporary CSV file
@@ -109,7 +108,6 @@ func captureOutput(f func()) string {
 	return buf.String()
 }
 
-
 func TestMain(m *testing.M) {
 	retCode := m.Run()
 
@@ -126,5 +124,3 @@ func TestMain(m *testing.M) {
 
 	os.Exit(retCode)
 }
-
-
